3fbecf54cafeaff3b99833bf8f35ae0258b17596
Jonas Bergman <jobe0900@student.miun.se>
2015-02-13 16:06:05 +0100

    Filled in test stubs for Behat

--- webapp/features/bootstrap/FeatureContext.php
+++ webapp/features/bootstrap/FeatureContext.php
@@ -6,11 +6,20 @@ use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 
+use PHPUnit_Framework_Assert as PHPUnit;
+use Symfony\Component\DomCrawler\Crawler;
+use Illuminate\Foundation\Testing\ApplicationTrait;
+
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext implements Context, SnippetAcceptingContext
 {
+    // get an application instance
+    use ApplicationTrait;
+
+    private $user;
+
     /**
      * Initializes context.
      *
@@ -23,11 +32,36 @@ class FeatureContext implements Context, SnippetAcceptingContext
     }
 
     /**
+     * @BeforeScenario
+     */
+    public function setUp()
+    {
+        if(!$this->app)
+        {
+            $this->refreshApplication();
+        }
+
+        $this->user = User::findOrFail(1);
+
+    }
+
+    /**
+     * Creates the application
+     * @return \Symfony\Component\HttpKernel\HttpKernelInterface
+     */
+    public function createApplication()
+    {
+        $unitTesting = true;
+        $testEnvironment = 'testing';
+        return require __DIR__.'/../../bootstrap/start.php';
+    }
+
+    /**
      * @Given I visit the URL of the web application
      */
     public function iVisitTheUrlOfTheWebApplication()
     {
-        throw new PendingException();
+        $this->call('GET', '/');
     }
 
     /**
@@ -35,14 +69,27 @@ class FeatureContext implements Context, SnippetAcceptingContext
      */
     public function iLogIn()
     {
-        throw new PendingException();
+        // cannot call with the stored hashed password...
+        $this->call('POST', 'login', array('email' => $this->user->email, 'password' => 'secret_pass'));
     }
 
     /**
-     * @Then I should see the text :arg1
+     * @Then I should see the text :welcomeText
      */
-    public function iShouldSeeTheText($arg1)
+    public function iShouldSeeTheText($welcomeText)
     {
-        throw new PendingException();
+        // actually get the username in the text string 
+        $userNameText = str_replace('{my_username}', $this->user->username, $welcomeText);
+
+        // ok manually redirect to the welcome page
+        // don't know how to get there without installing Mink and some more
+        $this->call('GET', 'welcome');
+
+        // examine the DOM to see if the correct text is there
+        $crawler = new Crawler($this->client->getResponse()->getContent());
+
+        $h2text = $crawler->filterXPath('//body/div/h2')->text();
+        PHPUnit::assertEquals($userNameText, $h2text);
     }
+
 }
